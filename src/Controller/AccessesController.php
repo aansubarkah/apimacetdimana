7be97c168b43d9481cc4ac283aec1542bb547e23
aansubarkah <aansubarkah@gmail.com>
2016-03-22 16:02:03 +0700

    add coordinate on access table

--- src/Controller/AccessesController.php
+++ src/Controller/AccessesController.php
@@ -79,6 +79,9 @@ class AccessesController extends AppController
 
             $dataToSave['active'] = 1;
 
+            $dataToSave['lat'] = $this->request->data['access']['lat'];
+            $dataToSave['lng'] = $this->request->data['access']['lng'];
+
             $access = $this->Accesses->newEntity($dataToSave);
             $this->Accesses->save($access);
             }
